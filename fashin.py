bcb88bd05b978c35e4ccbf40e3f1dbe1f0a037fe
AStupidPerson3141 <150556546+AStupidPerson3141@users.noreply.github.com>
2024-07-11 09:43:22 +0000

    no

--- fashin.py
+++ fashin.py
@@ -3,26 +3,30 @@ import numpy as np
 from keras import Sequential, layers, Input, initializers, optimizers, Initializer
 import visualkeras
 import json
+import matplotlib.pyplot as plt
 
 num_classes = 10
 input_shape = (28, 28, 1)
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+assert x_train.shape == (60000, 28, 28)
+assert x_test.shape == (10000, 28, 28)
+assert y_train.shape == (60000,)
+assert y_test.shape == (10000,)
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-
-
-# convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-print(x_train.shape)
-print(y_train.shape)
+plt.imshow(x_train[1], cmap='grey')
+plt.savefig("fash01.png")
+exit()
+
+print(x_train[1])
+if input("now:\n"):
+    pass
+print(y_train[1])
+if input("now:\n"):
+    pass
 
 model01 = Sequential([
     Input(shape=(28, 28, 1)),
@@ -40,20 +44,16 @@ model = Sequential([
     Input(shape=(28, 28, 1)),
     layers.Convolution2D(filters=2, kernel_size=(3, 3), activation=None, kernel_initializer=initializers.RandomNormal(stddev=0.01), ),
     layers.Dense(input_shape=(28, 28), units=784, kernel_initializer=initializers.RandomNormal(stddev=0.01),),
-    layers.Dense(input_shape=(28, 28), units=784, kernel_initializer=initializers.RandomNormal(stddev=0.01),),
-    layers.Activation('tanh'),
     layers.Flatten(), 
-    layers.Dense(5, kernel_initializer=initializers.RandomNormal(stddev=0.01),),
-    layers.Activation('relu'),
     layers.Dense(10, kernel_initializer=initializers.RandomNormal(stddev=0.01), ),
     layers.Activation('softmax'),
 ])
 
 
 model.compile(loss="categorical_crossentropy", optimizer="adamw", metrics=["accuracy"])
-visualkeras.layered_view(model, to_file='model01.png', max_xy=4000, max_z=800)
+visualkeras.layered_view(model, to_file='fs_model01.png', max_xy=4000, max_z=800)
 model.fit(x_train, y_train, batch_size=128, epochs=50)
-model.save("model01.keras")
+model.save("fs_model01.keras")
 
 
 print("finished")
